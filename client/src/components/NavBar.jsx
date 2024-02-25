a838e7e3624352ce8d3f64c21363aa01b1603f32
Paul Soteropulos <complexsimplicity22@yahoo.com>
2024-02-25 01:54:34 -0600

    finished

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,3 @@
-
 import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
@@ -11,10 +10,13 @@ const NavBar = () => {
     }
 
     return (
-        <div className='bg-success p-4 fluid' style={{ minHeight: '15vh', width: '100%' }}>
-            <div className='row justify-content-center align-items-center'>
-                <h1 className="text-white col mt-3">Creature Sanctuary</h1>
-                <div className='col mt-3'>
+        <div className='bg-success p-3 fluid' style={{ minHeight: '15vh', width: '100%' }}>
+            <div className='row g-0 justify-content-center align-items-center'>
+                <div className='row g-0 col-6 mt-3 align-items-center justify-content-center'>
+                    <img src="/unicorn.jpg" alt="unicorn logo" className='col-1' />
+                    <h1 className="col-6 text-white">Creature Sanctuary</h1>
+                </div>
+                <div className='col-6 mt-3'>
                     <NavLink to="/" className='h5 m-2' style={({ isActive }) => isActive ? activeStyle : inActiveStyle}>Home</NavLink>
                     <NavLink to="/new" className='h5 m-2' style={({ isActive }) => isActive ? activeStyle : inActiveStyle} end>Add Creature to Sanctuary</NavLink>
                 </div>
